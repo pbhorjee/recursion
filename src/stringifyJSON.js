df1dd0cd848721f1d085a4262d17c6c4e6882c15
pbhorjee <paul@bhorjee.com>
2015-09-28 06:46:14 -0500

    Add JSON Stringify method

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,33 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+    var result = '';
+
+    if (typeof obj === 'string') {
+        result = result.concat('"', String(obj), '"');
+    } else if (typeof obj === 'number' ||
+        typeof obj === 'boolean' ||
+        obj === null) {
+        result = result.concat(String(obj));
+    } else if (typeof obj === 'function' || typeof obj == 'undefined') {
+        //do nothing
+    } else if (Array.isArray(obj)) {
+        result = result.concat('[');
+        for (var i = 0; i < obj.length; i++) {
+            result = result.concat(stringifyJSON(obj[i]), ',');
+        }
+        if (result !== '[') { result = result.substring(0, result.length - 1); }
+        result = result.concat(']');
+    } else if (typeof obj === 'object') {
+        result = result.concat('{');
+        for (var k in obj) {
+            if (typeof obj[k] !== 'function' && typeof obj[k] !== 'undefined') {
+                result = result.concat('"', String(k), '"', ':', stringifyJSON(obj[k]), ',');
+            }
+        }
+        if (result !== '{') { result = result.substring(0, result.length - 1); }
+        result = result.concat('}');
+    }
+
+    return result;
 };
